b1f2a2ea7e4d7b81e2d7bf2756d2a0a3cde0cbf7
Vasilii <bogdanovva84@mail.ru>
2022-06-29 02:56:27 +0300

    Добавлена Задача 6

---
+++ DZ_Z6/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,  
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            Console.WriteLine("Введите число: ");
+            int a;
+                                    
+            a = int.Parse(Console.ReadLine());
+            
+            if (a % 2 == 0)            
+            {
+                Console.WriteLine("число четное");
+            }      
+            else
+            {
+                Console.WriteLine("число нечетное");
+            }                
+
+        }        
+    }            
+}
+
+
