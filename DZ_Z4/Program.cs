598d394301a0ffdf7bcfbf56794ddb43fb64687b
Vasilii <bogdanovva84@mail.ru>
2022-06-29 01:13:01 +0300

    Добавлена Задача 4

---
+++ DZ_Z4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт 
+// максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+//22 3 9 -> 22
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            int a = 44;
+            int b = 5;
+            int c = 78;            
+            int max = a;
+            
+            if (a>max) max = a;            
+            if (b>max) max = b;
+            if (c>max) max = c;            
+            
+
+            Console.Write("max = ");
+            Console.WriteLine(max);
+
+        }        
+    }            
+}
